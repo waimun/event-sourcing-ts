177691430c05306d1685b9bb56b11c667a884558
waimun <waimun@users.noreply.github.com>
2026-09-22 06:42:44 -0400

    guard current test coverage baseline in ci

--- modules/ships-n-cargo/vitest.config.mjs
+++ modules/ships-n-cargo/vitest.config.mjs
@@ -4,7 +4,13 @@ export default defineConfig({
   test: {
     include: ['src/**/*.test.ts', 'scripts/**/*.test.ts'],
     coverage: {
-      include: ['src/**/*.ts']
+      include: ['src/**/*.ts'],
+      thresholds: {
+        statements: 96.34,
+        branches: 91.92,
+        functions: 100,
+        lines: 95.89
+      }
     }
   }
 })
